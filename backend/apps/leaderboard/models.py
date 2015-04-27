931967832179fe88fd33ac2a8ee832e44a906cab
jaredmackey <mackeyja92@gmail.com>
2015-04-27 16:45:30 -0600

    Initial model setup.

--- backend/apps/leaderboard/models.py
+++ backend/apps/leaderboard/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class LeagueOfLegendsGameData(models.Model):
+    user_game_profile = models.ForeignKey(UserGameProfile)
+    time_stamp = models.DateTimeField()
+    kills = models.IntegerField()
+    deaths = models.IntegerField()
+    wins = models.IntegerField()
+    losses = models.IntegerField()
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=100)
+    platform = models.CharField(max_length=100)
+
+
+class UserGameProfile(models.Model):
+    game = models.ForeignKey(Game)
+    game_user_name = models.CharField(max_length=100)
+
+
+class UserProfile(models.Model):
+    user_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    games = models.ManyToManyField(UserGameProfile)
+    premium = models.BooleanField()
