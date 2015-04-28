4b3fc672bc3694ca2fd36730a2ed4942defeb530
jaredmackey <mackeyja92@gmail.com>
2015-04-28 14:28:32 -0600

    Reorged models.

--- backend/apps/leaderboard/models.py
+++ backend/apps/leaderboard/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 
 
-class LeagueOfLegendsGameData(models.Model):
-    user_game_profile = models.ForeignKey(UserGameProfile)
-    time_stamp = models.DateTimeField()
-    kills = models.IntegerField()
-    deaths = models.IntegerField()
-    wins = models.IntegerField()
-    losses = models.IntegerField()
-
-
 class Game(models.Model):
     name = models.CharField(max_length=100)
     platform = models.CharField(max_length=100)
@@ -18,6 +9,15 @@ class Game(models.Model):
 class UserGameProfile(models.Model):
     game = models.ForeignKey(Game)
     game_user_name = models.CharField(max_length=100)
+
+
+class LeagueOfLegendsGameData(models.Model):
+    user_game_profile = models.ForeignKey(UserGameProfile)
+    time_stamp = models.DateTimeField()
+    kills = models.IntegerField()
+    deaths = models.IntegerField()
+    wins = models.IntegerField()
+    losses = models.IntegerField()
 
 
 class UserProfile(models.Model):
