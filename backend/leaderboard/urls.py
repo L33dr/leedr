08eb99d751548b5cdc5bb3fe6f9c5cfebdb49beb
jaredmackey <mackeyja92@gmail.com>
2015-04-28 11:46:48 -0600

    Installed rest-auth urls.

--- backend/leaderboard/urls.py
+++ backend/leaderboard/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
+    url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
 ]
