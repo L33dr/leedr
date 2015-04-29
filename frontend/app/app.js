d0bbe2aee603ed6d89474c5392d1f60d05bb44d9
jaredmackey <mackeyja92@gmail.com>
2015-04-29 15:11:20 -0600

    Added Ng-view to index. Installed RestAngular to app

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -2,11 +2,12 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+    'ngRoute',
+    'restangular',
+    'ui.bootstrap'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    config(['$routeProvider', 'RestangularProvider', function ($routeProvider, RestangularProvider) {
+        $routeProvider.otherwise({redirectTo: '/view1'});
+        RestangularProvider.setBaseUrl('http://localhost:8001');
+        RestangularProvider.setRequestSuffix('/');
+    }]);
