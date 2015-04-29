abf62070fcbb9f25f549d63bc0e9dbd8b8da76e1
jaredmackey <mackeyja92@gmail.com>
2015-04-29 14:32:23 -0600

    Removed mixed in models.

--- backend/apps/leaderboard/models.py
+++ backend/apps/leaderboard/models.py
@@ -13,20 +13,6 @@ class UserGameProfile(models.Model):
     game_user_id = models.IntegerField()
 
 
-class LeagueOfLegendsGameData(models.Model):
-    user_game_profile = models.ForeignKey(UserGameProfile)
-    time_stamp = models.DateTimeField()
-    summoner_level = models.IntegerField()
-    total_champion_kills = models.IntegerField()
-    total_assists = models.IntegerField()
-    total_turrets_killed = models.IntegerField()
-    total_minions_killed = models.IntegerField()
-    avg_assists = models.IntegerField()
-    avg_champion_kills = models.IntegerField()
-    avg_deaths = models.IntegerField()
-    wins = models.IntegerField()
-
-
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     games = models.ManyToManyField(UserGameProfile)
