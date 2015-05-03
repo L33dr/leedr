90c4139cba7eff271a5325637362d35c0bab2ba8
jaredmackey <mackeyja92@gmail.com>
2015-05-03 17:33:52 -0600

    Fixed LOLGameSerializers to updated model names.

--- backend/apps/leaderboard/game_serializers/lol_serializers.py
+++ backend/apps/leaderboard/game_serializers/lol_serializers.py
@@ -1,24 +1,24 @@
 from rest_framework.serializers import ModelSerializer
-from apps.leaderboard.game_models.lol_game_model import LeagueOfLegendsGameData
-from apps.leaderboard.game_models.lol_game_model import AggregatedStats
-from apps.leaderboard.game_models.lol_game_model import PlayerStatSummary
+from apps.leaderboard.game_models.lol_game_model import LOLGameData
+from apps.leaderboard.game_models.lol_game_model import LOLAggregatedStats
+from apps.leaderboard.game_models.lol_game_model import LOLPlayerStatSummary
 
 
 class AggregatedStatsSerializer(ModelSerializer):
     class Meta:
-        model = AggregatedStats
+        model = LOLAggregatedStats
 
 
 class PlayerStatSummarySerializer(ModelSerializer):
     stats = AggregatedStatsSerializer(many=False, required=True)
 
     class Meta:
-        model = PlayerStatSummary
+        model = LOLPlayerStatSummary
 
 
 class LeagueOfLegendsGameDataSerializer(ModelSerializer):
     player_stat_summary = PlayerStatSummarySerializer(many=True, required=True)
 
     class Meta:
-        model = LeagueOfLegendsGameData
+        model = LOLGameData
         fields = ('player_stat_summary', 'time_stamp', 'summoner_id')
