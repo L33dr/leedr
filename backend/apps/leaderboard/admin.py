1d4b099c86988293d59d96bf022ef3f78de7d6a2
jaredmackey <mackeyja92@gmail.com>
2015-05-05 16:52:14 -0600

    Fixed models. Missing Charfield max lengths

--- backend/apps/leaderboard/admin.py
+++ backend/apps/leaderboard/admin.py
@@ -5,6 +5,22 @@ from models import UserGameProfile
 from game_models.lol_game_model import LOLGameData
 from game_models.lol_game_model import LOLPlayerStatSummary
 from game_models.lol_game_model import LOLAggregatedStats
+from apps.leaderboard.game_models.sc2_game_model import SC2Career
+from apps.leaderboard.game_models.sc2_game_model import Terran
+from apps.leaderboard.game_models.sc2_game_model import Zerg
+from apps.leaderboard.game_models.sc2_game_model import Protoss
+from apps.leaderboard.game_models.sc2_game_model import SwarmLevels
+from apps.leaderboard.game_models.sc2_game_model import Campaign
+from apps.leaderboard.game_models.sc2_game_model import Season
+from apps.leaderboard.game_models.sc2_game_model import RewardsList
+from apps.leaderboard.game_models.sc2_game_model import UserCategoryPoints
+from apps.leaderboard.game_models.sc2_game_model import UserAchievementPoints
+from apps.leaderboard.game_models.sc2_game_model import UserAchievement
+from apps.leaderboard.game_models.sc2_game_model import UserAchievementList
+from apps.leaderboard.game_models.sc2_game_model import SC2GameData
+from apps.leaderboard.game_models.sc2_game_model import Achievement
+from apps.leaderboard.game_models.sc2_game_model import Category
+from apps.leaderboard.game_models.sc2_game_model import ChildCategory
 
 admin.site.register(GameDetail)
 admin.site.register(UserProfile)
@@ -12,3 +28,20 @@ admin.site.register(UserGameProfile)
 admin.site.register(LOLGameData)
 admin.site.register(LOLPlayerStatSummary)
 admin.site.register(LOLAggregatedStats)
+admin.site.register(SC2Career)
+admin.site.register(Terran)
+admin.site.register(Zerg)
+admin.site.register(Protoss)
+admin.site.register(SwarmLevels)
+admin.site.register(Campaign)
+admin.site.register(Season)
+admin.site.register(RewardsList)
+admin.site.register(UserCategoryPoints)
+admin.site.register(UserAchievementPoints)
+admin.site.register(UserAchievement)
+admin.site.register(UserAchievementList)
+admin.site.register(SC2GameData)
+admin.site.register(Achievement)
+admin.site.register(Category)
+admin.site.register(ChildCategory)
+
--- backend/apps/leaderboard/game_models/sc2_game_model.py
+++ backend/apps/leaderboard/game_models/sc2_game_model.py
@@ -2,14 +2,21 @@ from django.db import models
 
 from apps.leaderboard.models import UserGameProfile
 
+######################################################################
+#
+# Basic profile information.
+# API Information: GET PROFILE /SC2/PROFILE/:ID/:REGION/:NAME/
+#
+######################################################################
+
 
 class SC2Career(models.Model):
-    primary_race = models.CharField()
+    primary_race = models.CharField(max_length=200)
     terran_wins = models.IntegerField()
     protoss_wins = models.IntegerField()
     zerg_wins = models.IntegerField()
-    highest_1v1_rank = models.CharField()
-    highest_team_rank = models.CharField()
+    highest_1v1_rank = models.CharField(max_length=200)
+    highest_team_rank = models.CharField(max_length=200)
     season_total_games = models.IntegerField()
     career_total_games = models.IntegerField()
 
@@ -50,8 +57,8 @@ class Campaign(models.Model):
         "hots" : "BRUTAL"
     }
     """
-    wol = models.CharField()
-    hots = models.CharField()
+    wol = models.CharField(max_length=200)
+    hots = models.CharField(max_length=200)
 
 
 class Season(models.Model):
@@ -68,8 +75,8 @@ class RewardsList(models.Model):
 
     That data will be stored in a separate class definition.
     """
-    selected = models.CommaSeparatedIntegerField()
-    earned = models.CommaSeparatedIntegerField()
+    selected = models.CommaSeparatedIntegerField(max_length=2500)
+    earned = models.CommaSeparatedIntegerField(max_length=2500)
 
 
 class UserCategoryPoints(models.Model):
@@ -82,6 +89,14 @@ class UserAchievementPoints(models.Model):
     category_points = models.ManyToManyField(UserCategoryPoints)
 
 
+class UserAchievementList(models.Model):
+    """
+    This is their total achievement points.
+    Each achievement will have a FK back to this class.
+    """
+    points = models.OneToOneField(UserAchievementPoints)
+
+
 class UserAchievement(models.Model):
     """
     API gives us an ID, we will have to resolve that later with another api call.
@@ -92,22 +107,14 @@ class UserAchievement(models.Model):
     user_list = models.ForeignKey(UserAchievementList)
 
 
-class UserAchievementList(models.Model):
-    """
-    This is their total achievement points.
-    Each achievement will have a FK back to this class.
-    """
-    points = models.OneToOneField(UserAchievementPoints)
-
-
 class SC2GameData(models.Model):
     user_game_profile = models.ForeignKey(UserGameProfile)
-    id = models.IntegerField()
+    profile_id = models.IntegerField()
     realm = models.IntegerField()
-    display_name = models.CharField()
-    clan_name = models.CharField()
-    clan_tag = models.CharField()
-    profile_path = models.CharField()
+    display_name = models.CharField(max_length=200)
+    clan_name = models.CharField(max_length=200)
+    clan_tag = models.CharField(max_length=200)
+    profile_path = models.CharField(max_length=200)
     portrait_url = models.URLField()
     career = models.OneToOneField(SC2Career)
     swarm_levels = models.OneToOneField(SwarmLevels)
@@ -120,11 +127,32 @@ class SC2GameData(models.Model):
         return str(self.user_game_profile) + "'s SC2 Game Data"
 
 
+####################################################################
+#
+# From achievements data only. Not used in basic profile data.
+# API information: GET ACHIEVEMENTS /SC2/DATA/ACHIEVEMENTS
+#
+####################################################################
+
+
 class Achievement(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=250)
-    id = models.IntegerField()
+    achievement_id = models.IntegerField()
     category = models.IntegerField()
     points = models.IntegerField()
     icon = models.URLField()
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    category_id = models.IntegerField()
+
+
+class ChildCategory(models.Model):
+    title = models.CharField(max_length=50)
+    category = models.IntegerField()
+    featured_achievement_id = models.IntegerField()
+    parent = models.ForeignKey(Category)
+
 
