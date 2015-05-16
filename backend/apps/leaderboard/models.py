c1f01ffce2dd4cc7119f94f8724b07c816bf126f
jaredmackey <mackeyja92@gmail.com>
2015-05-16 11:32:00 -0600

    Changed usergameprofile to have a foreignkey instead of userprofile to have a many to many field. This was allowing users to have multiple profiles per game profile.

--- backend/apps/leaderboard/models.py
+++ backend/apps/leaderboard/models.py
@@ -16,31 +16,39 @@ class GameDetail(models.Model):
     def __str__(self):
         return self.name + ' on ' + self.platform
 
-
-class UserGameProfile(models.Model):
-    """
-    Used to bind the user profile to each game the user participates in.
-    """
-    game = models.ForeignKey(GameDetail)
-    game_user_name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.game_user_name + "'s " + self.game.name + ' profile'
-
 class UserProfile(models.Model):
     """
     Base user profile. This will bind to a registered user and bind all the related data to them.
-    Premium field is not yet used. Will be implemented later as a paid service.
+    The premium field is currently used to filter down data returned to a user in the game views.
+    If they are not premium they should only get one result. If they are premium they should get all the results.
+
+    The game profile will contain game specific user identification (Game username, Game user_id, etc).
     """
     user = models.OneToOneField(User)
     premium = models.BooleanField()
-    games = models.ManyToManyField(UserGameProfile)
+
+    def is_premium(self):
+        return self.premium
 
     def __str__(self):
         return self.user.get_username() + "'s Profile"
 
-@receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    """Create a matching profile whenever a user object is created."""
-    if created:
-        profile, new = UserProfile.objects.get_or_create(user=instance, premium=False)
+    @receiver(post_save, sender=User)
+    def create_profile(sender, instance, created, **kwargs):
+        """Create a matching profile whenever a user object is created."""
+        if created:
+            profile, new = UserProfile.objects.get_or_create(user=instance, premium=False)
+
+class UserGameProfile(models.Model):
+    """
+    Used to bind the user profile to each game the user participates in.
+    Related name field is for the serializers to relate back to there and get the list of all the games a user has.
+    """
+    game = models.ForeignKey(GameDetail)
+    game_user_name = models.CharField(max_length=100)
+    user = models.ForeignKey(UserProfile, related_name="games")
+
+    def __str__(self):
+        return self.game_user_name + "'s " + self.game.name + ' profile'
+
+
