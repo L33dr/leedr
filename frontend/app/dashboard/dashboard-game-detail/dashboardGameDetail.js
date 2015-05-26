4ca1ed1f651e077cc9d04feb08dc6966eec05f73
jaredmackey <mackeyja92@gmail.com>
2015-05-26 09:17:24 -0600

    Fixed null values on refreshing game detail view.

--- frontend/app/dashboard/dashboard-game-detail/dashboardGameDetail.js
+++ frontend/app/dashboard/dashboard-game-detail/dashboardGameDetail.js
@@ -9,31 +9,33 @@ angular.module('myApp.dashboardGameDetail', ['ngRoute'])
         });
     }])
 
-    .controller('DashboardGameDetailCtrl', ['$scope', '$routeParams', '$location', 'Restangular', 'gameService',
-        function ($scope, $routeParams, $location, Restangular, gameService) {
+    .controller('DashboardGameDetailCtrl', ['$scope', '$routeParams', '$location', '$timeout', 'Restangular', 'gameService',
+        function ($scope, $routeParams, $location, $timeout, Restangular, gameService) {
 
             $scope.templateUrl = 'dashboard/dashboard-game-views/' + $routeParams.gameShorthand + '.html';
 
             // Checks to see if the game in the route params is a game we support and that a user is logged in.
             // If so, it will get the url from the
             //      gameService and pull in the data and assign it to the scope.
+            $timeout(function () {
             if (gameService[$routeParams.gameShorthand] && $scope.user) {
-                Restangular.all(gameService[$routeParams.gameShorthand].url).customGET().then(function (data) {
-                    if (data[0]) {
-                        $scope.GameData = data[0];
 
-                    } else {
+                    Restangular.all(gameService[$routeParams.gameShorthand].url).customGET().then(function (data) {
+                        if (data[0]) {
+                            $scope.GameData = data[0];
+
+                        } else {
+                            $location.path("/dashboard");
+                            toastr.error("We are still updating your data. Please check back later.");
+                        }
+                    }, function (error) {
+                        toastr.error("Something went wrong retrieving your data. Here is the error message: " + error);
                         $location.path("/dashboard");
-                        toastr.error("We are still updating your data. Please check back later.");
-                    }
-                }, function (error) {
-                    toastr.error("Something went wrong retrieving your data. Here is the error message: " + error);
-                    $location.path("/dashboard");
-                });
-            } else if ($scope.user && !gameService[$routeParams.gameShorthand]){
+                    })
+            } else if ($scope.user && !gameService[$routeParams.gameShorthand]) {
                 // If the game is not in our supported list it will redirect them to the home page.
                 toastr.error("Could not find that game!");
                 $location.path('/dashboard');
             }
-
+            }, 250)
         }]);
